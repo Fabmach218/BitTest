653574981d0b977f54bd5c5326f6106e39916f18
Alvarden15 <alvaroracchumi@hotmail.com>
2020-01-24 11:00:29 -0500

    Commit 1

---
+++ BitTest/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BitTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
